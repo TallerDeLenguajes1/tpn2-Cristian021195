363f28cef809285f18a9a53b1f0d7ac1ec7b7edb
Cristian021195 <cristiangramajo015@gmail.com>
2020-04-05 20:04:23 -0300

    Primer Mensaje de Cambio

---
+++ tp2_1_1.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define N 4
+#define M 5
+
+int main(){
+int f,c;
+double mt[N][M];//matriz
+for(f = 0;f<N; f++){
+	for(f = 0;f<M; f++){
+		cprintf(“%lf ”, mt[f][c]);
+	}
+cprintf(“\n”);
+}
+
+
+return 0;
+}
+
+
